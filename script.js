9afc9a89b2fa35bd7047b5bac6f5702af2868bdc
Aria <112182ssss@example.com>
2025-03-23 13:08:50 +0800

    fix rwd

--- script.js
+++ script.js
@@ -1,9 +1,64 @@
-$(window).scroll(function(evt){
-  if ($(window).scrollTop()>0)
-    
- $(".navbar").removeClass("navbar-top");
-  else
- $(".navbar").addClass("navbar-top");
+// 導航欄變化
+$(window).scroll(function () {
+  if ($(window).scrollTop() > 0) {
+    $(".navbar").removeClass("navbar-top");
+  } else {
+    $(".navbar").addClass("navbar-top");
+  }
 });
 
-var s = skrollr.init();
+// GSAP 與 ScrollTrigger 設定
+gsap.registerPlugin(ScrollTrigger);
+
+// 標題動畫
+gsap.from(".title", {
+  y: -50,
+  opacity: 0,
+  duration: 1.5,
+  ease: "power2.out",
+  scrollTrigger: {
+    trigger: ".title",
+    start: "top 80%",
+    toggleActions: "play none none reverse",
+  },
+});
+
+// 內容區塊動畫
+gsap.from("#section_ask .img", {
+  x: -200,
+  opacity: 0,
+  duration: 1,
+  ease: "power2.out",
+  scrollTrigger: {
+    trigger: "#section_ask",
+    start: "top 80%",
+    toggleActions: "play none none reverse",
+  },
+});
+
+gsap.from("#section_about .col-sm-12", {
+  y: 50,
+  opacity: 0,
+  duration: 1.2,
+  ease: "power2.out",
+  scrollTrigger: {
+    trigger: "#section_about",
+    start: "top 85%",
+    toggleActions: "play none none reverse",
+  },
+});
+
+// 其他區塊動畫
+document.querySelectorAll("#section_about .col-md-4").forEach((el) => {
+  gsap.from(el, {
+    y: 50,
+    opacity: 0,
+    duration: 1.2,
+    ease: "power2.out",
+    scrollTrigger: {
+      trigger: el,
+      start: "top 85%",
+      toggleActions: "play none none reverse",
+    },
+  });
+});
